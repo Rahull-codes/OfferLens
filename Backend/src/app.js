7a058bafcc17268e960de309937dd151d3e6ec78
Rahul sahu <sahu92083@gmail.com>
2026-07-25 18:46:51 +0530

    Update CORS origin to production URL

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://offer-lens-lime.vercel.app',
     credentials: true,
 }));
 
